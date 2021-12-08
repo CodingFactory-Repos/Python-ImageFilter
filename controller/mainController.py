e84cfb5c20a0c4460ddde1cea71939068d2442df
Loule | Louis <35641311+Loule95450@users.noreply.github.com>
2021-12-08 11:10:39 +0100

    🎞 - add a Gray filter

--- controller/mainController.py
+++ controller/mainController.py
@@ -1,8 +1,14 @@
-from controller import nameController  # import controller/nameController
-
+import cv2
 """
 This is the main controller of the program.
 This is the controller that will be used to control the program.
 """
 
-print(f"Hi {nameController.get_name_of_user()}, welcome to PythonBones")  # This is a test to see if the program is working.
+image = cv2.imread('./data/images/dubai.jpg')
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+cv2.imshow('Original image', image)
+cv2.imshow('Gray image', gray)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
