32db540b758e47354a8774c4b5e7a586e2eb1aa8
Loule | Louis <35641311+Loule95450@users.noreply.github.com>
2021-12-08 10:28:17 +0100

    🦴 - Add template

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+from controller import mainController  # Import the main controller in controller/mainController.py
 
-# Press ⌃F5 to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+"""
+Main file of the project.
+This file is the entry point of the project.
+There is no need to modify this file.
+"""
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+mainController  # Start the main controller
