b964107126b5c046b22bd8aad1ca34bd00605e3b
Loule | Louis <35641311+Loule95450@users.noreply.github.com>
2021-12-09 09:15:47 +0100

    🙅‍♀️ - If filters has been called

--- controller/mainController.py
+++ controller/mainController.py
@@ -18,43 +18,16 @@ for arg in args:
 
             image = cv2.imread(image_path)
 
-            if filters_list['blur'] != None:
+            if 'blur' in filters_list and filters_list['blur'] != None:
                 image = filterController.to_blur(image, int(filters_list['blur']))
                 print("[INFO] Blur filter applied")
 
-            if filters_list['grayscale'] != None:
+            if 'grayscale' in filters_list and filters_list['grayscale'] != None:
                 image = filterController.to_grayscale(image)
                 print("[INFO] Grayscale filter applied")
 
-            if filters_list['dilate'] != None:
+            if 'dilate' in filters_list and filters_list['dilate'] != None:
                 image = filterController.to_dilate(image, int(filters_list['dilate']))
                 print("[INFO] Dilate filter applied")
 
             imagesController.write_images(image, image_path)
-
-            # dilate_image = filterController.to_dilate(image, 10)
-            # imagesController.write_images(dilate_image, image.replace('.jpg', '_dilate.jpg'))
-            # print("[INFO] processed image to dilate {}/{}".format(i + 1, len(images_list)))
-
-# Get all the images from the folder
-
-# if args[1] == "--filter":
-#     if args[2].find("blur") != -1:
-#         level = args[2].split("blur:")[1].split("|")[0]
-#
-#         # Blur the images
-#         images = filterController.to_blur(images, level)
-#
-#     elif args[2].find("grayscale") != -1:
-#         # Convert the images to grayscale
-#         images = filterController.to_grayscale(images)
-#
-#     elif args[2].find("dilate") != -1:
-#         level = args[2].split("dilate:")[1].split("|")[0]
-#         # Dilate the images
-#         images = filterController.to_dilate(images)
-
-# for (i, image) in enumerate(images):
-#     dilate_image = filterController.to_dilate(image, 10)
-#     imagesController.write_images(dilate_image, image.replace('.jpg', '_dilate.jpg'))
-#     print("[INFO] processed image to dilate {}/{}".format(i + 1, len(images)))
